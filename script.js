671edb85260dfd616b573da0f253bd0644be0769
41andrew <jedrzej.malinowski@icloud.com>
2023-02-13 23:03:32 +0100

    Animation on scroll with IntersectionObserver

---
+++ script.js
@@ -0,0 +1,21 @@
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      const square = entry.target.querySelector('.square');
+      const heading = entry.target.querySelector('.heading');
+  
+      console.log('triggered');
+      if (entry.isIntersecting) {
+        square?.classList.add('square-animation');
+        heading?.classList.add('heading-animation');
+        return; // if we added the class, exit the function
+      }
+  
+      // We're not intersecting, so remove the class!
+      square?.classList.remove('square-animation');
+      heading?.classList.remove('heading-animation');
+
+    });
+  });
+
+observer.observe(document.querySelector('.square-wrapper'));
+observer.observe(document.querySelector('.one'));
