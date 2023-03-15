589893ba05afe50b3b474341e87deb2497633943
41andrew <jedrzej.malinowski@icloud.com>
2023-03-15 23:15:10 +0100

    Bugfixes

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
-const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-      const square = entry.target.querySelector('.square');
-      const heading = entry.target.querySelector('.heading');
-  
-      console.log('triggered');
-      if (entry.isIntersecting) {
-        square?.classList.add('square-animation');
-        heading?.classList.add('heading-animation');
-        return; // if we added the class, exit the function
-      }
-  
-      // We're not intersecting, so remove the class!
-      square?.classList.remove('square-animation');
-      heading?.classList.remove('heading-animation');
+let options = {
+  threshold: 0.1
+}
 
-    });
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    const square = entry.target.querySelector('.square');
+    const heading = entry.target.querySelector('.heading_name');
+
+    console.log('triggered');
+    if (entry.isIntersecting) {
+      square?.classList.add('square-animation');
+      heading?.classList.add('heading-animation');
+      return; // if we added the class, exit the function
+    }
+
+    // We're not intersecting, so remove the class!
+    //square?.classList.remove('square-animation');
+    heading?.classList.remove('heading-animation');
+
   });
+}, options);
 
 observer.observe(document.querySelector('.square-wrapper'));
 observer.observe(document.querySelector('.one'));
